76326dd4de48d403da37f582634dd9ac21d016a2
blinky3713 <blinky3713@gmail.com>
2018-06-12 10:00:13 -0400

    add type annotations for client

--- src/Network/IPFS/Client.hs
+++ src/Network/IPFS/Client.hs
@@ -7,4 +7,8 @@ import           Network.IPFS.API.V0.Types
 import           Servant.API               ((:<|>) (..))
 import           Servant.Client
 
+v0GetNode :: Maybe String -> ClientM IPFSNodeInfo
+v0GetObject :: String -> ClientM ByteString
+v0PostAddObjects :: [AddFile] -> ClientM [AddObjectResponse]
+
 v0GetNode :<|> v0GetObject :<|> v0PostAddObjects = client apiV0
